d569ad41ce4b25846147cce575c2445ce847cce2
colinplancich <colin.plancich@gmail.com>
2022-08-09 20:02:00 -0700

    Uploading files from local machine

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquierer');
+const fs = require('fs');
+
+// create an empty array list to store employee objects
+
+// gather manager data
+
+    // inquirer.prompt()
+
+    // THEN build a manager object
+
+// gather engineer data
+
+    // inquirer.prompt()
+
+    // THEN build an engineer object
+
+// gather intern data
+
+    // inquierer.prompt()
+
+    // THEN build an intern object
+
+// Ask user for which team member they want to add / if are they finished adding members
+
+    // inquirer.prompt
+
+    // THEN decide which function to call
+
+// Generate html & write file
+
+    // generateHTML()
+
+    // generateFile()
