de90cadc16ea389bb644e4ad4cfed45109ceaa85
colinplancich <colin.plancich@gmail.com>
2022-08-10 18:31:02 -0700

    Added template.js for HTML rendering

---
+++ src/template.js
@@ -0,0 +1,15 @@
+const generateHTML = (data) => {
+    const generateManager = (manager) => {
+        // return HTML template for manager card
+
+    }
+    const generateEngineer = (engineer) => {
+        // return HTML template for engineer card
+
+    }
+    const generateIntern = (intern) => {
+        // return HTML template for intern card
+
+    }
+    // Final HTML element that collects user data and renders data to page
+}
